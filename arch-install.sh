4bb45fdf5d12489600502f8f8f14237661be5011
crolbar <61518558+crolbar@users.noreply.github.com>
2023-05-18 00:05:30 +0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -27,11 +27,11 @@ then
     parted /dev/$drive set 1 boot on
     parted /dev/$drive mkpart primary linux-swap 128MiB 8GiB
     parted /dev/$drive mkpart primary ext4 8GiB 100%
-    mkfs.fat -F32 /dev/$drive
-    mkswap /dev/$drive
-    mkfs.ext4 /dev/$drive
-    mount /dev/$drive /mnt
-    mount -m /dev/$drive /mnt/boot/efi
+    mkfs.fat -F32 /dev/$drive'1'
+    mkswap /dev/$drive'2'
+    mkfs.ext4 /dev/$drive'3'
+    mount /dev/$drive'3' /mnt
+    mount -m /dev/$drive'1' /mnt/boot/efi
     pacstrap /mnt base base-devel linux linux-firmware grub nano networkmanager efibootmgr
  elif [[ $ef == bios ]];
 then
@@ -40,11 +40,11 @@ then
     parted /dev/$drive set 1 boot on
     parted /dev/$drive mkpart primary linux-swap 128MiB 8GiB
     parted /dev/$drive mkpart primary ext4 8GiB 100%
-    mkfs.ext4 /dev/$drive
-    mkswap /dev/$drive
-    mkfs.ext4 /dev/$drive
-    mount /dev/$drive /mnt
-    mount -m /dev/$drive /mnt/boot
+    mkfs.ext4 /dev/$drive'1'
+    mkswap /dev/$drive'2'
+    mkfs.ext4 /dev/$drive'3'
+    mount /dev/$drive'3' /mnt
+    mount -m /dev/$drive'1' /mnt/boot
     pacstrap /mnt base base-devel linux linux-firmware grub nano networkmanager
  else 
     exit
