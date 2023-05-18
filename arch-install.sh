3d8afcddf81295c3be902c7de110e0e9404e12bb
crolbar <61518558+crolbar@users.noreply.github.com>
2023-05-18 10:52:54 +0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -66,3 +66,24 @@ arch-chroot /mnt passwd
 arch-chroot /mnt passwd $user
 umount -R /mnt
 echo "you can reboot now"
+echo "do you want to use my bspwm config"
+read confirmation2
+if [[ confirmation2 == yes]]
+then
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+yay -S --needed bspwm sxhkd picom dmenu dunst polybar pulseaudio zsh pavucontrol neofetch alacritty lsd ttf-jetbrains-mono-nerd ttf-font-awesome ttf-roboto papirus-icon-theme xclip polkit-gnome materia-theme lxappearance network-manager-applet xfce4-power-manager nerd-fonts-complete-starship thunar feh firefox xorg-xrandr xorg-xinput xorg-server xorg-xinit gedit ly
+systemctl enable ly.service
+cp -R $HOME/bspwm/bspwm $HOME/.config/
+cp -R $HOME/bspwm/alacritty $HOME/.config/
+cp -R $HOME/bspwm/neofetch $HOME/.config/
+cp -R $HOME/bspwm/zsh $HOME/.config/
+cp -R $HOME/bspwm/.zshrc $HOME/
+elif [[confirmation2 == no ]]
+then
+exit
+else 
+ exit
+fi 
+
+
+
