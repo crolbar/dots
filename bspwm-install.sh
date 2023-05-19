d76a08bf94e627892b1e4c0355db1087d4d3617b
crolbar <61518558+crolbar@users.noreply.github.com>
2023-05-19 16:46:35 +0300

    Update bspwm-install.sh

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-yay -S --needed bspwm sxhkd picom dmenu dunst polybar pulseaudio zsh pavucontrol neofetch alacritty lsd ttf-jetbrains-mono-nerd ttf-font-awesome ttf-roboto papirus-icon-theme xclip polkit-gnome materia-theme lxappearance network-manager-applet xfce4-power-manager nerd-fonts-complete-starship thunar feh firefox xorg-xrandr xorg-xinput xorg-server xorg-xinit gedit ly
+yay -S --needed bspwm sxhkd picom dmenu dunst polybar pulseaudio zsh pavucontrol neofetch alacritty lsd ttf-jetbrains-mono-nerd ttf-font-awesome ttf-roboto papirus-icon-theme xclip polkit-gnome materia-theme lxappearance network-manager-applet xfce4-power-manager thunar feh firefox xorg-xrandr xorg-xinput xorg-server xorg-xinit gedit ly
 systemctl enable ly.service
 cp -R $HOME/bspwm/bspwm $HOME/.config/
 cp -R $HOME/bspwm/alacritty $HOME/.config/
