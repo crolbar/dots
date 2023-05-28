8d3cdf43f53468741b560c9b1b210b5e4ead46c3
crolbar <mni05@abv.bg>
2023-05-28 04:19:29 +0300

    hypr

--- hypr/scripts/default-sink.sh
+++ hypr/scripts/default-sink.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # changes the default output device
-other_sink=$(pacmd list-sinks | awk -v current="$current_sink" '/index:/{i=$0} /index:/{if ($0!=current && !/\* /) { sub(/^index: /, ""); print $2 }}')
+other_sink=$(pacmd list-sinks | awk '/index:/{i=$0} /index:/{if (!/\* /) { sub(/^index: /, ""); print $2; exit }}')
     pacmd set-default-sink "$other_sink"
