968057f534b1d843e1e3440c5b5b89196a1f0b71
crolbar <mni05@abv.bg>
2023-05-28 09:20:51 +0300

    hypr

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # crolbar
 sudo -u $user bash << EOF
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
+git clone https://aur.archlinux.org/yay.git /home/$user/ && cd /home/$user/yay && makepkg -si --noconfirm
 yay -Sy --needed --noconfirm bspwm sxhkd picom dmenu dunst polybar pulseaudio alsa-utils zsh pavucontrol neofetch alacritty lsd ttf-hack ttf-font-awesome ttf-roboto dracula-icons-git polkit-gnome dracula-gtk-theme network-manager-applet xfce4-power-manager thunar feh firefox xorg-xrandr xorg-xinput xorg-server xorg-xinit gedit ly mesa physlock
 sudo systemctl enable ly.service
 cd /dots
