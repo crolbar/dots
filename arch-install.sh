a8caa6da25b9e41de5c17ffc2839f04fbe97d91d
crolbar <61518558+crolbar@users.noreply.github.com>
2023-05-31 03:29:28 +0300

    Add files via upload

--- arch-install.sh
+++ arch-install.sh
@@ -82,8 +82,8 @@ echo "
 prompt="    >"
 while IFS= read -p "$prompt" -r -s -n 1 char
 do
-    if [[ $char == $'\0' ]]
-    then
+    if [[ $char == $'\0' ]]; then
+
         break
     fi
     prompt='*'
@@ -99,8 +99,7 @@ echo "
 prompt="    >"
 while IFS= read -p "$prompt" -r -s -n 1 char
 do
-    if [[ $char == $'\0' ]]
-    then
+    if [[ $char == $'\0' ]]; then
         break
     fi
     prompt='*'
@@ -116,109 +115,29 @@ echo "
 read -p "    >" WM
 
 # unmount devices if any
-umount -a /mnt
-
+umount -a
 echo
 
 #   create the partitions depending if using swap or not and bios or efi
-
-# if  [[ $ef == efi ]] && [[ $swap == y || $swap == yes || $swap == "" ]]; 
-# then
-#         parted /dev/$drive mklabel gpt
-#         parted /dev/$drive mkpart ESP fat32 0% 128MiB
-#         parted /dev/$drive set 1 boot on
-#         parted /dev/$drive mkpart primary linux-swap 128MiB $ss'iB'
-#         parted /dev/$drive mkpart primary ext4 $ss'iB' 100%
-#         mkfs.fat -F32 /dev/$drive'1'
-#         mkswap /dev/$drive'2'
-#         mkfs.ext4 /dev/$drive'3'
-#         swapon /dev/$drive'2'
-#         mount /dev/$drive'3' /mnt
-#         mount -m /dev/$drive'1' /mnt/boot/efi
-#         pacstrap /mnt base base-devel linux linux-firmware grub nano networkmanager efibootmgr
-#         echo	"
-#         =======================================================================================================================
-#         FORMATED /dev/$drive AND CREATED A $ef BOOT PARTITION WITH AN SWAP PARTITION WITH $ss OF STORAGE AND A ROOT PARTITION
-#         ======================================================================================================================="
- 
-#  elif [[ $ef == bios ]] && [[ $swap == y || $swap == yes || $swap == "" ]];
-#  then
-#         parted /dev/$drive mklabel msdos
-#         parted /dev/$drive mkpart primary ext4 0% 128MiB
-#         parted /dev/$drive set 1 boot on
-#         parted /dev/$drive mkpart primary linux-swap 128MiB $ss'iB'
-#         parted /dev/$drive mkpart primary ext4 $ss'iB' 100%
-#         mkfs.ext4 /dev/$drive'1'
-#         mkswap /dev/$drive'2'
-#         mkfs.ext4 /dev/$drive'3'
-#         swapon /dev/$drive'2'
-#         mount /dev/$drive'3' /mnt
-#         mount -m /dev/$drive'1' /mnt/boot
-#         pacstrap /mnt base base-devel linux linux-firmware grub nano networkmanager
-#         echo	"
-#         =======================================================================================================================
-#         FORMATED /dev/$drive AND CREATED A $ef BOOT PARTITION WITH AN SWAP PARTITION WITH $ss OF STORAGE AND A ROOT PARTITION
-#         ======================================================================================================================="
- 
-#  elif [[ $ef == efi ]] && [[ $swap != y || $swap != yes || $swap != "" ]]
-#  then
-#         parted /dev/$drive mklabel gpt
-#         parted /dev/$drive mkpart ESP fat32 0% 128MiB
-#         parted /dev/$drive set 1 boot on
-#         parted /dev/$drive mkpart primary ext4 128MiB 100%
-#         mkfs.fat -F32 /dev/$drive'1'
-#         mkfs.ext4 /dev/$drive'2'
-#         mount /dev/$drive'2' /mnt
-#         mount -m /dev/$drive'1' /mnt/boot/efi
-#         pacstrap /mnt base base-devel linux linux-firmware grub nano networkmanager efibootmgr
-#         echo	"
-#         ==================================================================================================================
-#                         FORMATED /dev/$drive AND CREATED A $ef BOOT PARTITION WITH AN ROOT PARTITION
-#         =================================================================================================================="
- 
-#  elif [[ $ef == bios ]] && [[ $swap != y || $swap != yes || $swap != "" ]];
-#  then
-#         parted /dev/$drive mklabel msdos
-#         parted /dev/$drive mkpart primary ext4 0% 128MiB
-#         parted /dev/$drive set 1 boot on
-#         parted /dev/$drive mkpart primary ext4 128MiB 100%
-#         mkfs.ext4 /dev/$drive'1'
-#         mkfs.ext4 /dev/$drive'2'
-#         mount /dev/$drive'2' /mnt
-#         mount -m /dev/$drive'1' /mnt/boot
-#         pacstrap /mnt base base-devel linux linux-firmware grub nano networkmanager
-#         echo	"
-#         ==================================================================================================================
-#                         FORMATED /dev/$drive AND CREATED A $ef BOOT PARTITION WITH AN ROOT PARTITION
-#         =================================================================================================================="
-#  else 
-#         echo "
-#         =================================================
-#                     EXITING OUT OF ARCH-INSTALL
-#         ================================================="
-#         exit
-# fi
-
-
 if [[ $ef == efi ]]; then 
     parted /dev/$drive mklabel gpt
     parted /dev/$drive mkpart ESP fat32 0% 500MiB
     parted /dev/$drive set 1 boot on
     mkfs.fat -F32 /dev/$drive'1'
-    mount -m /dev/$drive'1' /mnt/boot/efi
     parted /dev/$drive mkpart primary ext4 $ss'iB' 100%
     mkfs.ext4 /dev/$drive'2'
     mount /dev/$drive'2' /mnt
+    mount -m /dev/$drive'1' /mnt/boot/efi
     pacstrap /mnt base base-devel linux linux-firmware grub networkmanager efibootmgr
 elif [[ $ef == bios ]]; then
     parted /dev/$drive mklabel msdos
     parted /dev/$drive mkpart primary ext4 0% 500MiB
     parted /dev/$drive set 1 boot on
     mkfs.ext4 /dev/$drive'1'
-    mount -m /dev/$drive'1' /mnt/boot
     parted /dev/$drive mkpart primary ext4 $ss'iB' 100%
     mkfs.ext4 /dev/$drive'2'
     mount /dev/$drive'2' /mnt
+    mount -m /dev/$drive'1' /mnt/boot
     pacstrap /mnt base base-devel linux linux-firmware grub networkmanager
 else
     echo "
@@ -236,18 +155,19 @@ fi
 
 # configuring the system
 genfstab -U -p /mnt >> /mnt/etc/fstab 
-arch-chroot /mnt /bin/bash -c "systemctl enable NetworkManager"
-arch-chroot /mnt /bin/bash -c "grub-install /dev/$drive"
-arch-chroot /mnt /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
-arch-chroot /mnt /bin/bash -c "echo en_US.UTF-8 UTF-8 >> /etc/locale.gen"
-arch-chroot /mnt /bin/bash -c "echo LANG="C.UTF-8" > /etc/locale.conf"
-arch-chroot /mnt /bin/bash -c "locale-gen"
-arch-chroot /mnt /bin/bash -c "ln -sf /usr/share/zoneinfo/Europe/Sofia /etc/localtime" 
-arch-chroot /mnt /bin/bash -c "echo $host > /etc/hostname"
-arch-chroot /mnt /bin/bash -c "useradd -mg wheel $user"
-arch-chroot /mnt /bin/bash -c "echo '%wheel ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers"
-arch-chroot /mnt /bin/bash -c "echo '$user:$userpasswd' | chpasswd"
-arch-chroot /mnt /bin/bash -c "echo 'root:$rootpasswd' | chpasswd"
+arch-chroot /mnt /bin/bash -c " \
+systemctl enable NetworkManager && \
+grub-install /dev/$drive && \
+grub-mkconfig -o /boot/grub/grub.cfg && \
+echo en_US.UTF-8 UTF-8 >> /etc/locale.gen && \
+echo LANG="C.UTF-8" > /etc/locale.conf && \
+locale-gen && \
+ln -sf /usr/share/zoneinfo/Europe/Sofia /etc/localtime && \
+echo $host > /etc/hostname && \
+useradd -mg wheel $user && \
+echo '%wheel ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers && \
+echo '$user:$userpasswd' | chpasswd && \
+echo 'root:$rootpasswd' | chpasswd"
 echo "
         =========================================================================================
             ENABLED "NetworkManager" CHOSE EN_US FOR THE LOCALES AND /EU/SA FOR THE LOCALTIME
@@ -258,10 +178,7 @@ echo
 # GUI setup
 export user
 if [[ $WM == y || $WM == yes || $WM == "" ]]; then
-    arch-chroot /mnt /bin/bash -c "sudo pacman -Sy --noconfirm git && git clone https://github.com/crolbar/dots"
-    arch-chroot /mnt /bin/bash -c "/dots/bspwm-install.sh"
-else
-    echo
+    arch-chroot /mnt /bin/bash -c "sudo pacman -Sy --noconfirm git && git clone https://github.com/crolbar/dots && /dots/bspwm-install.sh"
 fi
 
 # umounting 
