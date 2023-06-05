3a96a6bab32eca86435cea7755c72370a92a0287
crolbar <mni05@abv.bg>
2023-06-05 14:46:17 +0300

    hypr

---
+++ hypr-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# crolbar
+sudo -u $user bash << EOF
+git clone https://aur.archlinux.org/yay.git /home/$user/yay && cd /home/$user/yay && makepkg -si --noconfirm
+yay -Sy --needed --noconfirm hyprland swaybg jq dunst waybar pulseaudio alsa-utils zsh pavucontrol neofetch alacritty lsd ttf-hack ttf-font-awesome ttf-roboto dracula-icons-git polkit-gnome dracula-gtk-theme network-manager-applet xfce4-power-manager thunar feh firefox gedit ly physlock
+sudo systemctl enable ly.service
+cd /dots
+cp -a hypr /home/$user/.config/ 
+cp -a alacritty /home/$user/.config/ 
+cp -a zsh /home/$user/.config/ 
+cp .zshrc /home/$user/
+EOF
