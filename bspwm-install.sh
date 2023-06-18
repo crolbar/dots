0ac3e75090e2203a8872479e640ebf242eff7f06
crolbar <61518558+crolbar@users.noreply.github.com>
2023-06-18 14:12:07 +0300

    Add files via upload

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 # crolbar
+case $1 in
+"")
+if command -v yay >/dev/null 2>&1; then
+    pac=yay
+elif command -v paru >/dev/null 2>&1; then
+    pac=paru
+else
+    git clone https://aur.archlinux.org/yay.git /home/$user/yay && cd /home/$user/yay && makepkg -si --noconfirm
+    pac=yay
+fi
+$pac -Sy --needed --noconfirm bspwm sxhkd picom dmenu dunst polybar pulseaudio alsa-utils zsh pavucontrol neofetch alacritty lsd ttf-hack ttf-font-awesome ttf-roboto dracula-icons-git polkit-gnome dracula-gtk-theme network-manager-applet xfce4-power-manager thunar feh xorg-xrandr xorg-xinput xorg-server xorg-xinit   
+cd /dots
+cp -a bspwm /home/$user/.config/ 
+cp -a alacritty /home/$user/.config/ 
+cp -a zsh /home/$user/.config/
+cp -a gtk-3.0 /home/$user/.config/  
+cp .zshrc /home/$user/
+;;
+
+arch-install)
 sudo -u $user bash << EOF
 git clone https://aur.archlinux.org/yay.git /home/$user/yay && cd /home/$user/yay && makepkg -si --noconfirm
 yay -Sy --needed --noconfirm bspwm sxhkd picom dmenu dunst polybar pulseaudio alsa-utils zsh pavucontrol neofetch alacritty lsd ttf-hack ttf-font-awesome ttf-roboto dracula-icons-git polkit-gnome dracula-gtk-theme network-manager-applet xfce4-power-manager thunar feh firefox xorg-xrandr xorg-xinput xorg-server xorg-xinit mousepad ly physlock
@@ -11,3 +31,6 @@ cp -a zsh /home/$user/.config/
 cp -a gtk-3.0 /home/$user/.config/  
 cp .zshrc /home/$user/
 EOF
+;;
+
+esac
--- hypr-install.sh
+++ hypr-install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 # crolbar
+case $1 in
+"")
+if command -v yay >/dev/null 2>&1; then
+    pac=yay
+elif command -v paru >/dev/null 2>&1; then
+    pac=paru
+else
+    git clone https://aur.archlinux.org/yay.git /home/$user/yay && cd /home/$user/yay && makepkg -si --noconfirm
+    pac=yay
+fi
+$pac -Sy --needed --noconfirm hyprland swaybg jq pamixer wofi dunst waybar pulseaudio alsa-utils zsh pavucontrol alacritty lsd ttf-hack ttf-font-awesome ttf-roboto dracula-icons-git polkit-gnome dracula-gtk-theme network-manager-applet thunar   
+cp -a hypr /home/$user/.config/ 
+cp -a alacritty /home/$user/.config/ 
+cp -a zsh /home/$user/.config/ 
+cp .zshrc /home/$user/
+;;
+
+arch-install)
 sudo -u $user bash << EOF
 git clone https://aur.archlinux.org/yay.git /home/$user/yay && cd /home/$user/yay && makepkg -si --noconfirm
 yay -Sy --needed --noconfirm hyprland swaybg jq pamixer wofi dunst waybar pulseaudio alsa-utils zsh pavucontrol neofetch alacritty lsd ttf-hack ttf-font-awesome ttf-roboto dracula-icons-git polkit-gnome dracula-gtk-theme network-manager-applet thunar feh firefox mousepad ly physlock
@@ -12,3 +30,6 @@ cp .zshrc /home/$user/
 echo 'GRUB_CMDLINE_LINUX_DEFAULT="nvidia_drm.modeset=1"' | sudo tee -a /etc/default/grub >> /dev/null
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 EOF
+;;
+
+esac
