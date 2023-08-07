8eb1b5eddb5c26e7c57db9647379e1c00a2c0fa5
crolbar <61518558+crolbar@users.noreply.github.com>
2023-08-07 05:40:40 +0300

    Add files via upload

--- hypr/scripts/dispatch.sh
+++ hypr/scripts/dispatch.sh
@@ -4,41 +4,40 @@ activemonitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true).name
 monitor_output=$(hyprctl monitors | awk '/Monitor/ {print $2}')
 
 # label the monitors to main, second and third
-declare -a monitor_names
+
 counter=1
 for monitor in $monitor_output; do
   if (( counter == 1 )); then
-    prefix="main"
-  elif ((counter == 2)); then
-    prefix="second"
-  else
     prefix="third"
+  elif ((counter == 2)); then
+    prefix="main"
+  else
+    prefix="second"
   fi
   
   declare "$prefix=$monitor"
-  monitor_names+=("$prefix")
   (( counter++ ))
 done
 
-
 case $2 in 
-f)
-# focus to a workspace depending on the focused monitor
-if [ $activemonitor == $main ]; then
-  hyprctl dispatch workspace "$1"
-elif [ $activemonitor == $second ]; then
-  hyprctl dispatch workspace $(($1 + 5))
-elif [ $activemonitor == $third ]; then
-  hyprctl dispatch workspace $(($1 + 10))
-fi;;
-s)
-# move a focused window to the selected workspace on the focused monitor
-if [ $activemonitor == $main ]; then
-  hyprctl dispatch movetoworkspacesilent "$1"
-elif [ $activemonitor == $second ]; then
-  hyprctl dispatch movetoworkspacesilent $(($1 + 5))
-elif [ $activemonitor == $third ]; then
-  hyprctl dispatch movetoworkspacesilent $(($1 + 10))
-fi;;
+  f)
+  # focus to a workspace depending on the focused monitor
+    case $activemonitor in 
+    $main)
+      hyprctl dispatch workspace "$1";;
+    $second)
+      hyprctl dispatch workspace $(($1 + 10));;
+    $third)
+      hyprctl dispatch workspace $(($1 + 20));;
+    esac;;
+  s)
+  # move a focused window to the selected workspace on the focused monitor
+    case $activemonitor in 
+    $main)
+      hyprctl dispatch movetoworkspacesilent "$1";;
+    $second)
+      hyprctl dispatch movetoworkspacesilent $(($1 + 10));;
+    $third)
+      hyprctl dispatch movetoworkspacesilent $(($1 + 20));;
+    esac;;
 esac
-
