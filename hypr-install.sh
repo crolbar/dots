7e24694e10c2a44792613460036a917c1d84da73
crolbar <61518558+crolbar@users.noreply.github.com>
2023-11-01 15:30:26 +0200

    Update hypr-install.sh

--- hypr-install.sh
+++ hypr-install.sh
@@ -19,16 +19,16 @@ case $1 in
         ;;
 
     arch-install)
-        sudo -u $user bash << EOF
-            git clone https://aur.archlinux.org/yay-bin.git /home/$user/yay && cd /home/$user/yay && makepkg -si --noconfirm
-            yay -Sy --needed --noconfirm hyprland swaybg jq pamixer wofi dunst waybar pulseaudio alsa-utils zsh pavucontrol neofetch alacritty lsd ttf-hack ttf-font-awesome ttf-roboto polkit-gnome network-manager-applet thunar feh brave-bin mousepad ly wl-clipboard swaylock nwg-look-bin
-            sudo systemctl enable ly.service
-            cd /dots
-            cp -a hypr /home/$user/.config/ 
-            cp -a alacritty /home/$user/.config/ 
-            cp -a zsh /home/$user/.config/ 
-            cp .zshrc /home/$user/
-        EOF
-        ;;
+sudo -u $user bash << EOF
+git clone https://aur.archlinux.org/yay-bin.git /home/$user/yay && cd /home/$user/yay && makepkg -si --noconfirm
+yay -Sy --needed --noconfirm hyprland swaybg jq pamixer wofi dunst waybar pulseaudio alsa-utils zsh pavucontrol neofetch alacritty lsd ttf-hack ttf-font-awesome ttf-roboto polkit-gnome network-manager-applet thunar feh brave-bin mousepad ly wl-clipboard swaylock nwg-look-bin
+sudo systemctl enable ly.service
+cd /dots
+cp -a hypr /home/$user/.config/ 
+cp -a alacritty /home/$user/.config/ 
+cp -a zsh /home/$user/.config/ 
+cp .zshrc /home/$user/
+EOF
+;;
 
 esac
