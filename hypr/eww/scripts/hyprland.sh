c90eb2e3255b6395c57226706245aab7207fd96f
crolbar <crolbar@crolbar.com>
2024-04-18 13:09:12 +0300

    eww systray

--- hypr/eww/scripts/hyprland.sh
+++ hypr/eww/scripts/hyprland.sh
@@ -2,11 +2,11 @@
 print() {
     workspaces=$(hyprctl workspaces -j | jq -c 'sort_by(.id)')
     active=$(hyprctl activeworkspace -j | jq '.id')
-    currwin=$(hyprctl activewindow -j | jq -r '.title' | cut -c1-90)
+    currwin=$(hyprctl activewindow -j | jq -r '.title')
     kb_layout=$(hyprctl devices -j | jq '.keyboards[] | select(.name == "sino-wealth-bluetooth-keyboard") | .active_keymap')
 
     echo "{\"workspaces\": $workspaces, \"active\": $active, \"currwin\": \"$currwin\", \"kb_layout\": $kb_layout}"
 }
 print
 
-nc -U /tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock | while read -r _; do print; done
+socat -U - UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock | while read -r l; do print "$l"; done
