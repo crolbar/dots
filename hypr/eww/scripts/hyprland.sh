b227c39ef5cac96a2671406fbbfcd5c1c1a242dd
crolbar <crolbar@crolbar.com>
2024-07-18 20:38:58 +0300

    eww bar: hypr new sock path

--- hypr/eww/scripts/hyprland.sh
+++ hypr/eww/scripts/hyprland.sh
@@ -4,9 +4,21 @@ print() {
     active=$(hyprctl activeworkspace -j | jq '.id')
     currwin=$(hyprctl activewindow -j | jq -r '.title')
     kb_layout=$(hyprctl devices -j | jq '.keyboards[] | select(.name == "sino-wealth-bluetooth-keyboard") | .active_keymap')
+    
+    if [ -z "$kb_layout" ]; then
+        kb_layout=$(hyprctl devices -j | jq '.keyboards[] | select(.name == "crolbar-yuki") | .active_keymap')
+    fi
+
+    if [ -z "$kb_layout" ]; then
+        kb_layout="\"\""
+    fi
+
+    if echo $currwin | grep '\"'; then
+        currwin=$(echo $currwin | sed 's/"/\\"/g')
+    fi
 
     echo "{\"workspaces\": $workspaces, \"active\": $active, \"currwin\": \"$currwin\", \"kb_layout\": $kb_layout}"
 }
 print
 
-socat -U - UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock | while read -r l; do print "$l"; done
+socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock | while read -r l; do print "$l"; done
