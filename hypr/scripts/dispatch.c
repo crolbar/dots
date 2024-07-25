8443ca47bf250665e373e2d7a4a7e1f84d054ebd
crolbar <crolbar@crolbar.com>
2024-07-25 10:07:54 +0300

    hyprctl was slow for some reason so i made this shit

--- hypr/scripts/dispatch.c
+++ hypr/scripts/dispatch.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+
 const int ZERO_ASCII = 48;
 const int NINE_ASCII = 57;
 
@@ -17,6 +24,9 @@ int is_number(char* str) {
 
 int main(int argc, char** argv) 
 {
+    const char* HYPRLAND_INSTANCE_SIGNATURE = getenv("HYPRLAND_INSTANCE_SIGNATURE");
+    const char* XDG_RUNTIME_DIR = getenv("XDG_RUNTIME_DIR");
+    
     if (argc == 3)
     {
         char* wid = argv[1];
@@ -24,11 +34,45 @@ int main(int argc, char** argv)
         {
             int move_win_to = !strcmp(argv[2], "s");
 
-            char* fmt = "hyprctl dispatch %s %s";
+            int client_fd;
+            struct sockaddr_un addr;
+            client_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+            if (client_fd == -1) {
+                perror("socket");
+                exit(EXIT_FAILURE);
+            }
+            memset(&addr, 0, sizeof(struct sockaddr_un));
+            addr.sun_family = AF_UNIX;
+
+            //$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock
+            char* sock_path_fmt = "%s/hypr/%s/.socket.sock";
+            size_t sock_path_size = strlen(HYPRLAND_INSTANCE_SIGNATURE) + strlen(XDG_RUNTIME_DIR) + strlen(sock_path_fmt);
+            char* sock_path = malloc(sock_path_size);
+            sprintf(sock_path, sock_path_fmt, XDG_RUNTIME_DIR, HYPRLAND_INSTANCE_SIGNATURE);
+
+            strncpy(addr.sun_path, sock_path, sizeof(addr.sun_path) - 1);
+
+            if (connect(client_fd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1) {
+                perror("connect");
+                exit(EXIT_FAILURE);
+            }
+
+            char* fmt = "/dispatch %s %s";
             char* action = (move_win_to) ? "movetoworkspacesilent" : "workspace";
-            char* cmd = malloc(strlen(fmt) + strlen(action));
+            size_t size = strlen(fmt) + strlen(action);
+            char* cmd = malloc(size);
             sprintf(cmd, fmt, action, wid);
-            system(cmd);
+
+            write(client_fd, cmd, size);
+
+            //char buffer[100];
+            //int num_read = read(client_fd, buffer, sizeof(buffer));
+            //if (num_read > 0) {
+            //    printf("Client received: %s\n", buffer);
+            //}
+
+            free(cmd);
+            free(sock_path);
         } else {
             printf("workspace id (first arg) is not a number\n"); 
         }
