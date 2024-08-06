b0997594a829659ce1d400ea15af84a8a2164147
crolbar <crolbar@crolbar.com>
2024-08-06 08:03:47 +0300

    shared/nix: optimize ops & more caches

--- shared/nix.nix
+++ shared/nix.nix
@@ -4,20 +4,41 @@
 
   nix = {
     settings = {
-      experimental-features = ["nix-command" "flakes"];
-      auto-optimise-store = true;
-      cores = 8;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
 
+      auto-optimise-store = true;
+      min-free = "${toString (5 * 1024 * 1024 * 1024)}";
+      max-jobs = "auto";
+      warn-dirty = false;
+      http-connections = 30;
       builders-use-substitutes = true;
-      trusted-substituters = [ "https://anyrun.cachix.org" ];
-      substituters = [ "https://anyrun.cachix.org" ];
+
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://anyrun.cachix.org"
+      ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       ];
     };
 
-    gc.automatic = true;
-    gc.dates = "weekly";
-    gc.options = "--delete-older-than 15d";
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 15d";
+    };
+
+    optimise.automatic = true;
+
+    daemonIOSchedClass = "idle";
+    daemonCPUSchedPolicy = "idle";
   };
 }
