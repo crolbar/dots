fba59ce33b42c481c248cf2723091c5f2beb9cb7
crolbar <crolbar@crolbar.com>
2024-08-15 22:05:31 +0300

    home/gui/term_emulators/wezterm: disable glyph warnings & adjusting window size

--- home/gui/term_emulators/wezterm.nix
+++ home/gui/term_emulators/wezterm.nix
@@ -17,6 +17,8 @@
 
           window_background_opacity = 0.8,
           window_padding = { left = 0, right = 0, top = 0, bottom = 0, },
+          adjust_window_size_when_changing_font_size = false,
+          warn_about_missing_glyphs =  false,
 
 
 
