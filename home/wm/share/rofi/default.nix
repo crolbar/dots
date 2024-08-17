6d39026a1db1269630840c0a9a4e5916209bdb65
crolbar <crolbar@crolbar.com>
2024-08-17 04:31:23 +0300

    home/wm/share/rofi: set terminal to use

--- home/wm/share/rofi/default.nix
+++ home/wm/share/rofi/default.nix
@@ -1,7 +1,12 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.rofi = {
     enable = true;
     theme = "${config.xdg.configHome}/rofi/spotlight-dark.rasi";
+    terminal = "${pkgs.alacritty}/bin/alacritty";
   };
   xdg.configFile."rofi/spotlight-dark.rasi".source = ./spotlight-dark.rasi;
 }
