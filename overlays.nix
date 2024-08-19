6ad731de847252ff2aa6fc60561565b3500e529d
crolbar <crolbar@crolbar.com>
2024-08-19 09:57:56 +0300

    update flake lock & overlays

--- overlays.nix
+++ overlays.nix
@@ -1,32 +1,19 @@
 {
+  inputs',
   rust-overlay,
   nur,
-  dapu,
-  matm,
-  tt-rs,
-  lobster,
-  npassm,
-  swww,
   ...
 }: let
   ovrls =
     builtins.foldl' (acc: elem: acc // elem) {}
     (
-      map (pkg: let
-        pname = pkg.defaultPackage.x86_64-linux.pname;
-      in {
-        "${pname}" = pkg.defaultPackage.x86_64-linux;
-      })
-      [dapu matm tt-rs npassm]
+      map (pname: {
+        "${pname}" = inputs'."${pname}".packages.default;
+      }) ["dapu" "matm" "tt-rs" "npassm" "lobster" "swww"]
     );
 in {
   nixpkgs.overlays = [
-    (final: prev:
-      {
-        lobster = lobster.packages.x86_64-linux.default;
-        swww = swww.packages.x86_64-linux.swww;
-      }
-      // ovrls)
+    (final: prev: ovrls)
     rust-overlay.overlays.default
     nur.overlay # unused
   ];
