d88aceca1318aadbb097d9dbcf37a1540c33148e
crolbar <crolbar@crolbar.com>
2024-08-20 23:06:18 +0300

    hosts/screw/servers/nginx: don't hardcode grafana port

--- hosts/screw/servers/nginx.nix
+++ hosts/screw/servers/nginx.nix
@@ -1,5 +1,6 @@
 {config, ...}: let
   forgejo_port = config.services.forgejo.settings.server.HTTP_PORT;
+  grafana_port = config.services.grafana.settings.server.http_port;
 in {
   services.nginx = {
     enable = true;
@@ -10,7 +11,7 @@ in {
 
       locations."/dns" = {
         # TODO: do better
-        return = "http://screw.rs:9002";
+        return = "http://screw.rs:${toString grafana_port}";
       };
       locations."/" = {
         proxyPass = "http://127.0.0.1:${toString forgejo_port}";
