65400a23bba2a82ecf4f5572780d628b3c2ca644
crolbar <crolbar@crolbar.com>
2024-09-10 22:02:24 +0300

    home/cli/zzz: dapu up

--- home/cli/zzz/dapu.nix
+++ home/cli/zzz/dapu.nix
@@ -1,28 +1,24 @@
 {pkgs, ...}: {
   home.packages = [pkgs.dapu];
 
-  home.file."scripts/dapu.sh" = {
-    executable = true;
-    text = ''
-      #!${pkgs.runtimeShell}
-      output=$(dapu -o)
+  home.file."scripts/dapu.sh".source = pkgs.writers.writeBash "dapu.sh" ''
+    output=$(dapu -o)
 
-      if [[ -e $output ]]; then
-          if [[ -d $output ]]; then
-              cd "$output" || exit
-          else
-              cd "$(dirname "$output")" || exit
-          fi
-          if [[ $1 != "cd" ]]; then
-              if [[ -e $output/flake.nix ]]; then
-                  nix develop --command bash -c "SHELL=zsh && nvim ."
-              elif [[ -e $output/shell.nix ]]; then
-                  nix-shell --run "bash -c \"SHELL=zsh && nvim .\""
-              else
-                  nvim "$output"
-              fi
-          fi
-      fi
-    '';
-  };
+    if [[ -e $output ]]; then
+        if [[ -d $output ]]; then
+            cd "$output" || exit
+        else
+            cd "$(dirname "$output")" || exit
+        fi
+        if [[ $1 != "cd" ]]; then
+            if [[ -e $output/flake.nix ]]; then
+                nix develop --command bash -c "SHELL=zsh && nvim ."
+            elif [[ -e $output/shell.nix ]]; then
+                nix-shell --run "bash -c \"SHELL=zsh && nvim .\""
+            else
+                nvim "$output"
+            fi
+        fi
+    fi
+  '';
 }
