e6c9e365ec09bee07e77abdf3c8b5e9fcbe4a298
crolbar <crolbar@crolbar.com>
2024-09-15 21:49:55 +0300

    home/cli/git: add git-cliff

--- home/cli/git.nix
+++ home/cli/git.nix
@@ -1,15 +1,32 @@
 {...}: {
-  programs.git = {
-    enable = true;
+  programs = {
+    git = {
+      enable = true;
 
-    extraConfig = {
-      init = {defaultBranch = "master";};
-      user = {
-        name = "crolbar";
-        email = "crolbar@crolbar.com";
+      extraConfig = {
+        init = {defaultBranch = "master";};
+        user = {
+          name = "crolbar";
+          email = "crolbar@crolbar.com";
+        };
+        url."https://github.com/".insteadOf = ["gh:"];
+        credential.helper = "store";
       };
-      url."https://github.com/".insteadOf = ["gh:"];
-      credential.helper = "store";
+    };
+
+    git-cliff = {
+      enable = true;
+      settings = {
+        git = {
+          filter_unconventional = false;
+          commit_parsers = [
+            {
+              message = ".*";
+              group = "Other";
+            }
+          ];
+        };
+      };
     };
   };
 }
