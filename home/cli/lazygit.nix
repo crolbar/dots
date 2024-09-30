fffb9173dc0f5f65eb37fdb832091ecfdc1b8075
crolbar <crolbar@crolbar.com>
2024-09-30 22:28:08 +0300

    home/cli/lazygit: scrolloff & '-' bind

--- home/cli/lazygit.nix
+++ home/cli/lazygit.nix
@@ -4,11 +4,17 @@
     settings = {
       gui = {
         scrollPastBottom = false;
+        scrollOffMargin = 15;
         nerdFontsVersion = "3";
         splitDiff = "always";
       };
       git = {
         autoFetch = false;
+      };
+      keybinding = {
+        universal = {
+          prevScreenMode = "-";
+        };
       };
     };
   };
