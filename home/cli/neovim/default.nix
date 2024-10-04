8fe33bec65ea850015822e1fe72b8290b21e2c8a
crolbar <crolbar@crolbar.com>
2024-10-04 16:14:31 +0300

    home/cli/neovim: add blade queries

--- home/cli/neovim/default.nix
+++ home/cli/neovim/default.nix
@@ -8,6 +8,25 @@
     recursive = true;
   };
 
+  xdg.configFile."nvim/after/queries/blade/highlights.scm".text = ''
+    (directive) @function
+    (directive_start) @function
+    (directive_end) @function
+    (comment) @comment
+    ((parameter) @include (#set! "priority" 110))
+    ((php_only) @include (#set! "priority" 110))
+    ((bracket_start) @function (#set! "priority" 120))
+    ((bracket_end) @function (#set! "priority" 120))
+    (keyword) @function
+  '';
+
+  xdg.configFile."nvim/after/queries/blade/injections.scm".text = ''
+    ((text) @injection.content
+        (#not-has-ancestor? @injection.content "envoy")
+        (#set! injection.combined)
+        (#set! injection.language php))
+  '';
+
   home.packages = with pkgs; [
     neovim.unwrapped
     ripgrep
