280bfb921ea5d50141d5f23f2038eed498c099c0
crolbar <crolbar@crolbar.com>
2024-10-12 00:13:21 +0300

    home/gui/zathura: theming

--- home/gui/zathura.nix
+++ home/gui/zathura.nix
@@ -1,8 +1,23 @@
-{
+let
+  bg = "#161616";
+  fg = "#CCCCCC";
+in {
   programs.zathura = {
     enable = true;
     options = {
       selection-clipboard = "clipboard";
+      statusbar-bg = bg;
+      statusbar-fg = fg;
+
+      notification-bg = bg;
+      notification-fg = fg;
+
+      default-bg = bg;
+      default-fg = fg;
+
+      recolor-lightcolor = bg;
+      recolor-darkcolor = fg;
+      recolor = true;
     };
     mappings = {
       f = "toggle_fullscreen";
