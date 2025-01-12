dccb68d66c8e0d86ea8d7d93f1860c010ac5a31d
crolbar <crolbar@crolbar.com>
2025-01-12 22:37:49 +0200

    home/gui/term/alacritty: up conf

--- home/gui/term_emulators/alacritty.nix
+++ home/gui/term_emulators/alacritty.nix
@@ -2,7 +2,7 @@
   programs.alacritty = {
     enable = true;
     settings = {
-      live_config_reload = true;
+      general.live_config_reload = true;
 
       window = {
         opacity = 0.8;
