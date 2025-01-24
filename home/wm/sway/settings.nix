612866d8daa41b0147ce573f9ab7af642dfb200d
crolbar <crolbar@crolbar.com>
2025-01-24 16:44:09 +0200

    home/wm/sway: some window rules

--- home/wm/sway/settings.nix
+++ home/wm/sway/settings.nix
@@ -3,51 +3,58 @@
   pkgs,
   ...
 }: {
-  wayland.windowManager.sway.config = {
-    modifier = "Mod4";
-    window.titlebar = false;
-    floating.titlebar = false;
-    gaps.smartBorders = "on";
+  wayland.windowManager.sway = {
+    extraConfig = ''
+      for_window [app_id="emacs" workspace="2"] border none
+      for_window [app_id="org.pulseaudio.pavucontrol"] floating enable
+    '';
 
-    startup = let
-      mk = cmd: {command = cmd;};
-    in [
-      (mk "${lib.getExe pkgs.playerctl} daemon")
-      (mk "~/scripts/wall.sh i")
-      (mk "${lib.getExe' pkgs.swww "swww-daemon"}")
-      (mk "${lib.getExe' pkgs.networkmanagerapplet "nm-applet"}")
-      (mk "${lib.getExe pkgs.dunst}")
-    ];
+    config = {
+      modifier = "Mod4";
+      window.titlebar = false;
+      floating.titlebar = false;
+      gaps.smartBorders = "on";
 
-    input = {
-      "*" = {
-        pointer_accel = "-0.9";
-        repeat_delay = "300";
-        repeat_rate = "50";
+      startup = let
+        mk = cmd: {command = cmd;};
+      in [
+        (mk "${lib.getExe pkgs.playerctl} daemon")
+        (mk "~/scripts/wall.sh i")
+        (mk "${lib.getExe' pkgs.swww "swww-daemon"}")
+        (mk "${lib.getExe' pkgs.networkmanagerapplet "nm-applet"}")
+        (mk "${lib.getExe pkgs.dunst}")
+      ];
+
+      input = {
+        "*" = {
+          pointer_accel = "-0.9";
+          repeat_delay = "300";
+          repeat_rate = "50";
+        };
+        "5824:10203:crolbar_YUKI" = {
+          accel_profile = "adaptive";
+          pointer_accel = "-0.2";
+        };
+        "type:keyboard" = {
+          xkb_layout = "us,us,bg";
+          xkb_options = "grp:win_space_toggle";
+          xkb_variant = ",dvorak,phonetic";
+        };
       };
-      "5824:10203:crolbar_YUKI" = {
-        accel_profile = "adaptive";
-        pointer_accel = "-0.2";
+
+      output = {
+        "DP-1" = {
+          mode = "1920x1080@144Hz";
+          #mode = "1024x768@119.989Hz";
+          position = "0,0";
+          scale = "1.0";
+          transform = "normal";
+        };
       };
-      "type:keyboard" = {
-        xkb_layout = "us,us,bg";
-        xkb_options = "grp:win_space_toggle";
-        xkb_variant = ",dvorak,phonetic";
-      };
+
+      bars = [
+        {statusCommand = "${lib.getExe pkgs.i3status}";}
+      ];
     };
-
-    output = {
-      "DP-1" = {
-        mode = "1920x1080@144Hz";
-        #mode = "1024x768@119.989Hz";
-        position = "0,0";
-        scale = "1.0";
-        transform = "normal";
-      };
-    };
-
-    bars = [
-      {statusCommand = "${lib.getExe pkgs.i3status}";}
-    ];
   };
 }
