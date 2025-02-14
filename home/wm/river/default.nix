d0a81bed1aedbf4a9edf6b1e54618ad7733c00be
crolbar <crolbar@crolbar.com>
2025-02-14 13:19:23 +0200

    home/wm/river: remove gtk title bars

--- home/wm/river/default.nix
+++ home/wm/river/default.nix
@@ -16,6 +16,18 @@
     lswt # shows title & app-id of views
   ];
 
+  xdg.configFile."gtk-3.0/gtk.css".text = ''
+    headerbar.titlebar.default-decoration {
+      background: transparent;
+      opacity: 0;
+      margin: 0 0 -35px 0;
+    }
+    window.csd,
+    window.csd decoration {
+      box-shadow: none;
+    }
+  '';
+
   wayland.windowManager.river = {
     enable = true;
 
