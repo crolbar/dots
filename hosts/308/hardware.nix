843b92fb929a94afceb16b855eaeb1791e60d14b
crolbar <crolbar@crolbar.com>
2025-04-05 01:38:40 +0300

    hosts/308/hardware: trying out nvidia 570.133

--- hosts/308/hardware.nix
+++ hosts/308/hardware.nix
@@ -7,7 +7,21 @@
   hardware = {
     bluetooth.enable = false;
     nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      #package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "570.133.07";
+        sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY=";
+        sha256_aarch64 = "sha256-yTovUno/1TkakemRlNpNB91U+V04ACTMwPEhDok7jI0=";
+        openSha256 = "sha256-9l8N83Spj0MccA8+8R1uqiXBS0Ag4JrLPjrU3TaXHnM=";
+        settingsSha256 = "sha256-XMk+FvTlGpMquM8aE8kgYK2PIEszUZD2+Zmj2OpYrzU=";
+        persistencedSha256 = "sha256-G1V7JtHQbfnSRfVjz/LE2fYTlh9okpCbE4dfX9oYSg8=";
+
+        #version = "560.35.03";
+        #sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
+        #settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
+        #persistencedSha256 = lib.fakeSha256;
+      };
       nvidiaSettings = true;
       open = false;
       modesetting.enable = true;
