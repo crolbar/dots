a25aa48813ab8dd3527eea96d0f5653fe7b98437
crolbar <crolbar@crolbar.com>
2025-04-06 09:33:05 +0300

    home/gui/spotify: remove beautifulLyrics, frying my cpu

--- home/gui/spotify.nix
+++ home/gui/spotify.nix
@@ -8,18 +8,11 @@
   in {
     enable = true;
 
-    spotifyPackage = pkgs.spotify.overrideAttrs (old: {
-      commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
-    });
-
     enabledExtensions = with spicePkgs.extensions; [
-      shuffle
       adblock
-      bookmark
       groupSession
       songStats
       betterGenres
-      beautifulLyrics
       popupLyrics
       fullScreen
     ];
