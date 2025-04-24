fe9c9443faa2f95bfbbb7b225a445e8ef9aeda9f
crolbar <crolbar@crolbar.com>
2025-04-24 14:28:43 +0300

    home/cli/starship: disable cloud modules

--- home/cli/starship.nix
+++ home/cli/starship.nix
@@ -2,6 +2,9 @@
   programs.starship = {
     enable = true;
     settings = {
+      aws.disabled = true;
+      gcloud.disabled = true;
+      azure.disabled = true;
       git_status = {
         modified = "✶";
         staged = "✓";
