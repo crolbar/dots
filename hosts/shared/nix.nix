0ceb4e0e47a4ff300f2a14dc1509f6cc8674d65a
crolbar <crolbar@crolbar.com>
2025-05-12 13:34:28 +0300

    hosts/shared/nix: remove unused substituters

--- hosts/shared/nix.nix
+++ hosts/shared/nix.nix
@@ -25,16 +25,10 @@
       # the next system rebuild and will not be in the system rebuild that they are added
       substituters = [
         "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://hyprland.cachix.org"
-        "https://anyrun.cachix.org"
         "https://crolbar.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
         "crolbar.cachix.org-1:THfEkGfnXN+9c95nD3TAS8lGE27rDeFt/ZmNCiin4Is="
       ];
     };
