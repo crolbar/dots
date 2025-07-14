8a3d47b7ebc726fa15558366ee3254944fc04a26
crolbar <crolbar@crolbar.com>
2025-07-14 21:32:45 +0300

    .github/workflows: up-repo-tree

--- devShell.nix
+++ devShell.nix
@@ -5,22 +5,32 @@
     lib,
     ...
   }: let
-    pre-commit = pkgs.writers.writeBashBin "pre-commit.sh" ''
-      README="README.md"
-      VAL="$(tree --dirsfirst --gitignore | sed 's/$/\\n/' | tr -d '\n')"
+    pre-commit = let
+      tree = lib.getExe pkgs.tree;
+      sed = lib.getExe' pkgs.gnused "sed";
+      tr = lib.getExe' pkgs.coreutils-full "tr";
+      awk = lib.getExe' pkgs.gawk "awk";
+    in
+      pkgs.writers.writeBashBin "pre-commit.sh"
+      ''
+        export LANG=en_US.UTF-8
+        export LC_ALL=
 
-      CODE_BLOCK_START="\`\`\`nix"
-      CODE_BLOCK_END="\`\`\`"
+        README="README.md"
+        VAL="$(${tree} --dirsfirst --gitignore | ${sed} 's/$/\\n/' | ${tr} -d '\n')"
 
-      awk -v start="$CODE_BLOCK_START" -v end="$CODE_BLOCK_END" -v tree_output="$VAL" '
-      BEGIN {in_block = 0}
-      $0 == start {in_block = 1; print $0; print tree_output; next}
-      $0 == end && in_block {in_block = 0; print $0; next}
-      in_block == 0 {print $0}
-      ' "$README" > tmp && mv tmp "$README"
+        CODE_BLOCK_START="\`\`\`nix"
+        CODE_BLOCK_END="\`\`\`"
 
-      git add $README
-    '';
+        ${awk} -v start="$CODE_BLOCK_START" -v end="$CODE_BLOCK_END" -v tree_output="$VAL" '
+        BEGIN {in_block = 0}
+        $0 == start {in_block = 1; print $0; print tree_output; next}
+        $0 == end && in_block {in_block = 0; print $0; next}
+        in_block == 0 {print $0}
+        ' "$README" > tmp && mv tmp "$README"
+
+        git add $README
+      '';
   in {
     devShells.default = pkgs.mkShellNoCC {
       name = "dots";
