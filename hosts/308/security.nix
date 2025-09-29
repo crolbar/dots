7387084238128d919c785147325c70932a74d98a
crolbar <crolbar@crolbar.com>
2025-09-30 01:27:02 +0300

    hosts/308/security: fix i3lock not recognizing pass

--- hosts/308/security.nix
+++ hosts/308/security.nix
@@ -2,6 +2,9 @@
   security = {
     sudo.wheelNeedsPassword = false;
     rtkit.enable = true;
-    pam.services.swaylock = {};
+    pam.services = {
+      swaylock = {};
+      i3lock.enable = true;
+    };
   };
 }
